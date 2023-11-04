d707c0785bb9ed848e8b2ca5c6f112eada9dcab9
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2023-11-04 08:45:42 -0500

    adding datetime helper

--- app/libs/core/src/core.module.ts
+++ app/libs/core/src/core.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
 
-@Module({
-  providers: [],
-  exports: [],
-})
+@Module({})
 export class CoreModule {}
--- app/libs/core/src/index.ts
+++ app/libs/core/src/index.ts
@@ -1,2 +1,4 @@
 export * from './models';
 export * from './helpers';
+export * from './helpers';
+export * from './models';
